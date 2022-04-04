4cd334ec94582130fea1ca1c81bf1e0845b1c69f
Taylor Coons <taylorcoons8798@gmail.com>
2022-04-03 22:30:07 -0600

    Fix release_name script

--- release_name.sh
+++ release_name.sh
@@ -3,14 +3,8 @@
 VERSION=$(git describe --tags --abbrev=0)
 DEB_VERSION=$(echo $VERSION | sed 's/v//' | sed 's/\./-/2')
 
-sed -i "s/VERSION_TAG/$DEB_VERSION/" deb/DEBIAN/control
-
 GOARCH=amd64
 GOOS=linux
-
-go build
-
-cp custom-prompt deb/usr/local/bin
 
 DEB_FOLDER="custom-prompt_${DEB_VERSION}_${GOARCH}"
 
